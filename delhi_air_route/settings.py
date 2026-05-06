5686d492c28c698d5c4a3473195bdeafd331ef49
VishalGNV <vishal.22bce8243@vitapstudent.ac.in>
2026-05-06 16:22:24 +0530

    map not showing in website

--- delhi_air_route/settings.py
+++ delhi_air_route/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -116,3 +117,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # API Keys
 ORS_API_KEY = config("ORS_API_KEY")
 WAQI_API_KEY = config("WAQI_API_KEY")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
